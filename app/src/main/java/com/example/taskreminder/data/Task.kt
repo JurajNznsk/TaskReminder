69e786f14c8832d7833959f28e0de75206428f28
JurajNznsk <juraj.niznansky@gmail.com>
2025-10-08 21:19:56 +0200

    TaskItem: repaired / reskinned

---
+++ app/src/main/java/com/example/taskreminder/data/Task.kt
@@ -0,0 +1,6 @@
+package com.example.taskreminder.data
+
+data class Task(
+    val title: String,
+    val description: String
+)
--- app/src/main/java/com/example/taskreminder/ui/screens/TodoScreen.kt
+++ app/src/main/java/com/example/taskreminder/ui/screens/TodoScreen.kt
@@ -1,22 +1,29 @@
 package com.example.taskreminder.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,11 +34,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.taskreminder.R
+import com.example.taskreminder.data.Task
 import com.example.taskreminder.ui.theme.DarkGray
 import com.example.taskreminder.ui.theme.LightGray
 
 @Composable
 fun TodoScreen() {
+    // TODO: Make task list load from local database
+    val tasks = listOf(
+        Task("MAS", "Predavacky"),
+        Task("SI", "Nic"),
+        Task("ZTS", "DU")
+    )
+
+
     Scaffold (
         topBar = { TopAppBar() },
         bottomBar = { BottomAppBar() },
@@ -79,6 +95,8 @@ fun TodoScreen() {
                         )
                     }
                 }
+
+                TaskList(tasks)
             }
         }
 
@@ -103,8 +121,81 @@ fun TopAppBar() {
 }
 
 @Composable
-fun TaskList() {
-    // TODO: Lazy Column of Tasks
+fun TaskItem(
+    task: Task,
+    onDoneClick: () -> Unit,
+    onStarClick: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 16.dp
+            )
+            .background(
+                color = Color.Black,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .height(80.dp)
+    ) {
+        IconButton(
+            onClick = onDoneClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = stringResource(R.string.done_task),
+                tint = Color.White
+            )
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = task.title,
+                color = Color.White
+            )
+            Text(
+                text = task.description,
+                color = Color.White
+            )
+        }
+
+        Spacer(Modifier.weight(1f))
+
+        IconButton(
+            onClick = onStarClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = stringResource(R.string.star_task),
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun TaskList(
+    tasks: List<Task>
+) {
+   LazyColumn(
+       modifier = Modifier
+           .fillMaxSize(),
+       contentPadding = PaddingValues(vertical = 8.dp)
+   ) {
+       items(tasks) { task ->
+           TaskItem(
+               task = task,
+               onDoneClick = {},
+               onStarClick = {}
+           )
+           Spacer(modifier = Modifier.height(4.dp))
+       }
+   }
 }
 
 @Composable
