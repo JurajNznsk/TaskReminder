90782e277b417f8c888586b2a45f945bded634a3
JurajNznsk <juraj.niznansky@gmail.com>
2025-10-09 14:22:47 +0200

    Insert option added

--- app/src/main/java/com/example/taskreminder/data/TasksDao.kt
+++ app/src/main/java/com/example/taskreminder/data/TasksDao.kt
@@ -1,12 +1,17 @@
 package com.example.taskreminder.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface TasksDao {
     @Query("SELECT * FROM tasks")
     suspend fun getAllTasks(): List<Task>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
 
     @Query("DELETE FROM tasks WHERE id = :taskId")
     suspend fun deleteTaskById(taskId: Int)
